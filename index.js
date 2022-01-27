16e3bb1d29c732a9f084d26f074a62ac35e986a4
dasom037 <dasom037@gmail.com>
2022-01-27 17:12:49 +0900

    Object Destruncturing #1

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
 
 
 
-///////////////////////////////////////// Template String
+/////////////////////////////////////////
 
-let template = `내려써도
-괜찮아`;
 
-console.log(template);
 
+/////////////////////////////////////////
+
+
+
+/////////////////////////////////////////
+
+
+
+///////////////////////////////////////// Object Destruncturing #1
+{
+var exam = {
+    kor:20,
+    eng:30,
+    math:40
+};
+
+// function printExam(exam) {
+function printExam({kor, eng, math}) {
+// 
+
+
+    // // Destruncturing 
+    // let kor = exam.kor;
+    // let eng = exam.eng;
+    // let math = exam.math;
+
+    // let {kor, eng, math} = exam;
+
+    console.log("---- print() ")
+    console.log(`${kor}, ${eng}, ${math}`);
+    // console.log(`${total}`);
+}
+
+printExam(exam);
+}
+
+///////////////////////////////////////// 5. 향상된 json 객체 표현식 #2
+
+// let attr = "kor";
+// let exam = {
+//     [attr]:10
+// };
+
+// console.log(exam.kor);
+
+
+
+///////////////////////////////////////// 5. 향상된 json 객체 표현식 #1
+// {
+// let kor = 30;
+// let eng = 40;
+// let math = 50;
+
+// // var exam = {
+// //     kor:kor,
+// //     eng:eng,
+// //     math:math
+// // };
+
+// let exam = {kor, eng, math, total(){
+//     return 10;
+// }};
+
+// console.log(exam.kor)
+// console.log(exam.total())
+
+
+// }
+
+///////////////////////////////////////// 4. Template String
+
+// {
+// let title = "ES6";
+// let content = "새로운 기능 좋아";
+
+// let template = ` ${title} 내려써도
+// 괜찮아 ${content}`;
+
+
+// // let template2 = "철산도서관 \
+// //                      5층 노트북 \
+// //                      31 번";
+
+
+// console.log(template);
+
+// // 문자열 그대로 출력
+// let template2 = String.raw` ${title} 내려써도 \n\n\n
+// 괜찮아 ${content}`;
+
+// console.log(template2);
+
+// }
 
 
 

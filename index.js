5feb4b8211e5e7c4e1f581a5923ccd0620ac2e40
dasom037 <dasom037@gmail.com>
2022-01-27 15:44:35 +0900

    es6 add file

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+/////////////////////////////////////////
+
+{
+    var x = 30;
+}
+
+console.log(x);
+
+///////////////////////////////////////// 
+// let x = 30;
+// console.log(x);
+
+// var x = 40; //  Identifier 'x' has already been declared
+// console.log(x);

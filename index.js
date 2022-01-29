3068c399e581db470b718ee3c76faca7f94767d4
dasom037 <dasom037@gmail.com>
2022-01-29 13:06:40 +0900

    finished renewal

--- index.js
+++ index.js
@@ -1,147 +1,144 @@
 
-
 ///////////////////////////////////////// 22 - 속성 은닉화를 위한 쉬운 Getters/Setters 지원
 // https://youtu.be/cw5WNsfonhg
 /////////////////////////////////////////
 
-// class Exam {
+{
+    // class Exam {
 
-//     #kor; // private 선언(#)
-//     #eng;
-//     #math;
-//     static #info = "안녕하세요. Exam 클래스 입니다."  // static 전역변수;
+    //     #kor; // private 선언(#)
+    //     #eng;
+    //     #math;
+    //     static #info = "안녕하세요. Exam 클래스 입니다."  // static 전역변수;
 
-//     constructor(kor, eng, math) {
-//         this.#kor = kor; // 인스턴스 속성(필드)
-//         this.#eng = eng;
-//         this.#math = math;
-//     }
+    //     constructor(kor, eng, math) {
+    //         this.#kor = kor; // 인스턴스 속성(필드)
+    //         this.#eng = eng;
+    //         this.#math = math;
+    //     }
 
-//     get kor() {
-//         return this.#kor
-//     }
+    //     get kor() {
+    //         return this.#kor
+    //     }
 
-//     set kor(value) {
-//         this.#kor = value;
-//     }
+    //     set kor(value) {
+    //         this.#kor = value;
+    //     }
 
-//     #total() {
-//         return this.#kor + this.#eng + this.#math;
-//     }
+    //     #total() {
+    //         return this.#kor + this.#eng + this.#math;
+    //     }
 
-//     avg() {
-//         return this.#total() / 3.0;
-//     }
+    //     avg() {
+    //         return this.#total() / 3.0;
+    //     }
 
-//     // static 전역변수
-//     static info() {
-//         return Exam.#info; 
-//     }
-// }
-
-
-// let exam1 = new Exam();
-// exam1.kor = 30;
-// console.log(exam1.kor);
+    //     // static 전역변수
+    //     static info() {
+    //         return Exam.#info; 
+    //     }
+    // }
 
 
-
-
+    // let exam1 = new Exam();
+    // exam1.kor = 30;
+    // console.log(exam1.kor);
+}
 
 ///////////////////////////////////////// 21 - static 멤버 정의하기
 // https://youtu.be/AipNBFZQJTQ
 /////////////////////////////////////////
 
-// 외부에 노출하지 않기
+{
+    // 외부에 노출하지 않기
 
-// class Exam {
+    // class Exam {
 
-//     #kor; // private 선언(#)
-//     #eng;
-//     #math;
-//     static #info = "안녕하세요. Exam 클래스 입니다."  // static 전역변수;
+    //     #kor; // private 선언(#)
+    //     #eng;
+    //     #math;
+    //     static #info = "안녕하세요. Exam 클래스 입니다."  // static 전역변수;
 
-//     constructor(kor, eng, math) {
-//         this.#kor = kor; // 인스턴스 속성(필드)
-//         this.#eng = eng;
-//         this.#math = math;
-//     }
+    //     constructor(kor, eng, math) {
+    //         this.#kor = kor; // 인스턴스 속성(필드)
+    //         this.#eng = eng;
+    //         this.#math = math;
+    //     }
 
-//     #total() {
-//         return this.#kor + this.#eng + this.#math;
-//     }
+    //     #total() {
+    //         return this.#kor + this.#eng + this.#math;
+    //     }
 
-//     avg() {
-//         return this.#total() / 3.0;
-//     }
+    //     avg() {
+    //         return this.#total() / 3.0;
+    //     }
 
-//     // static 전역변수
-//     static info() {
-//         return Exam.#info; 
-//     }
-// }
+    //     // static 전역변수
+    //     static info() {
+    //         return Exam.#info; 
+    //     }
+    // }
 
-// // let exam = new Exam(10, 20, 30);
+    // // let exam = new Exam(10, 20, 30);
 
-// let exam1 = new Exam(10, 20, 30);
-// // let exam2 = new Exam(1, 2, 3);
-// // let exam3 = new Exam(1, 22, 33);
-
-
-// console.log("info : " + Exam.info());
-
-// console.log("total : " + exam.avg());
-// console.log("info : " + exam.info());
-// console.log("total : " + exam.total());
-// Uncaught TypeError: exam.total is not a function
+    // let exam1 = new Exam(10, 20, 30);
+    // // let exam2 = new Exam(1, 2, 3);
+    // // let exam3 = new Exam(1, 22, 33);
 
 
-// console.log(exam.kor); // undefined
-// Private field '#kor' must be declared in an enclosing class
+    // console.log("info : " + Exam.info());
+
+    // console.log("total : " + exam.avg());
+    // console.log("info : " + exam.info());
+    // console.log("total : " + exam.total());
+    // Uncaught TypeError: exam.total is not a function
 
 
+    // console.log(exam.kor); // undefined
+    // Private field '#kor' must be declared in an enclosing class
 
-
-
+}
 
 ///////////////////////////////////////// 20 - class 멤버 은닉하기
 // https://youtu.be/GOjtMeodZiY
 /////////////////////////////////////////
 
-// 외부에 노출하지 않기
+{
 
-// class Exam {
+    // 외부에 노출하지 않기
 
-//     #kor; // private 선언(#)
-//     #eng;
-//     #math;
+    // class Exam {
 
-//     constructor(kor, eng, math) {
-//         this.#kor = kor;
-//         this.#eng = eng;
-//         this.#math = math;
-//     }
+    //     #kor; // private 선언(#)
+    //     #eng;
+    //     #math;
 
-//     #total() {
-//         return this.#kor + this.#eng + this.#math;
-//     }
+    //     constructor(kor, eng, math) {
+    //         this.#kor = kor;
+    //         this.#eng = eng;
+    //         this.#math = math;
+    //     }
 
-//     avg() {
-//         return this.#total() / 3.0;
-//     }
-// }
+    //     #total() {
+    //         return this.#kor + this.#eng + this.#math;
+    //     }
 
-// let exam = new Exam(10, 20, 30);
-// console.log("total : " + exam.avg());
-// console.log("total : " + exam.total());
-// Uncaught TypeError: exam.total is not a function
+    //     avg() {
+    //         return this.#total() / 3.0;
+    //     }
+    // }
 
-
-// console.log(exam.kor); // undefined
-// Private field '#kor' must be declared in an enclosing class
+    // let exam = new Exam(10, 20, 30);
+    // console.log("total : " + exam.avg());
+    // console.log("total : " + exam.total());
+    // Uncaught TypeError: exam.total is not a function
 
 
+    // console.log(exam.kor); // undefined
+    // Private field '#kor' must be declared in an enclosing class
 
+
+}
 
 ///////////////////////////////////////// 19 - class를 이용한 객체 정의
 // https://youtu.be/e-OsENI4m7Q
@@ -154,282 +151,279 @@
  * - class 생성자는 new를 통하지 않고는 호출할 수 없다.
  */
 
-// // 기존
-// let proto = {kor:10, eng:30};
+{
+    // // 기존
+    // let proto = {kor:10, eng:30};
 
-// function Exam() {
+    // function Exam() {
 
-// }
+    // }
 
-// Exam.prototype = proto;
-// let exam = new Exam();
-// console.log(exam.kor);
+    // Exam.prototype = proto;
+    // let exam = new Exam();
+    // console.log(exam.kor);
 
-// class Exam {
-//     constructor(kor, eng, math) {
-//         this.kor = kor;
-//         this.eng = eng;
-//         this.math = math;
-//     }
+    // class Exam {
+    //     constructor(kor, eng, math) {
+    //         this.kor = kor;
+    //         this.eng = eng;
+    //         this.math = math;
+    //     }
 
-//     total() {
-//         return this.kor + this.eng + this.math;
-//     }
+    //     total() {
+    //         return this.kor + this.eng + this.math;
+    //     }
 
-//     avg() {
-//         return this.total() / 3.0;
-//     }
-// }
+    //     avg() {
+    //         return this.total() / 3.0;
+    //     }
+    // }
 
-// let exam = new Exam(10, 20, 30);
-// console.log("total : " + exam.total());
-
-
-
-
+    // let exam = new Exam(10, 20, 30);
+    // console.log("total : " + exam.total());
+}
 
 ///////////////////////////////////////// 18 - Arrow Functions와 그 특징
 // https://youtu.be/DpIoo-oNQBs
 /////////////////////////////////////////
 
-// No this
-// No arguments
-// No super
-// No new.target
+{
+    // No this
+    // No arguments
+    // No super
+    // No new.target
 
-// function print() {
-//     this.kor = 10;
-//     console.log("print : " + window.kor);
-// }
-
-
-// function Exam() {
-//     this.kor = 0;
-//     this.eng = 0;
-//     this.math = 0;
-// }
-
-// print();
-// let exam = new Exam();
-// console.log(exam.kor);
-// console.log(kor);
+    // function print() {
+    //     this.kor = 10;
+    //     console.log("print : " + window.kor);
+    // }
 
 
-// // 함수를 즉석에서 정의해서 넘기는 경우가 많다.
-// let nums = [13, 4, 6, 1, 3, 26];
+    // function Exam() {
+    //     this.kor = 0;
+    //     this.eng = 0;
+    //     this.math = 0;
+    // }
 
-// // nums.sort();
-
-// // 
-// // function compare(x, y) {
-// //     return x-y;
-// // }
-// // nums.sort(compare);
-
-// nums.sort((x, y) => x-y);
-// console.log(nums);
+    // print();
+    // let exam = new Exam();
+    // console.log(exam.kor);
+    // console.log(kor);
 
 
+    // // 함수를 즉석에서 정의해서 넘기는 경우가 많다.
+    // let nums = [13, 4, 6, 1, 3, 26];
+
+    // // nums.sort();
+
+    // // 
+    // // function compare(x, y) {
+    // //     return x-y;
+    // // }
+    // // nums.sort(compare);
+
+    // nums.sort((x, y) => x-y);
+    // console.log(nums);
+
+}
 
 
 ///////////////////////////////////////// 17 - Default Value의 이전방식과 현재방식
 // https://youtu.be/i2WoD5BvDEs
 /////////////////////////////////////////
 
-// {
-//     console.log("hi - 5" || 2 || 5); // null, 빈값(""), undefined 등의 false로 인식
-// }
+{
+    // {
+    //     console.log("hi - 5" || 2 || 5); // null, 빈값(""), undefined 등의 false로 인식
+    // }
 
-// function add(x = 0, y = 0) {
-//     // x = x || 0;
-//     // y = y || 0;
-//     return x + y;
-// }
+    // function add(x = 0, y = 0) {
+    //     // x = x || 0;
+    //     // y = y || 0;
+    //     return x + y;
+    // }
 
-// // console.log(add(2, 3));
-// // console.log(add(1));
-
-
-// function add2(x, y = 10) {
-//     console.log("length : " + arguments.length);
-//     return x + y;
-// }
+    // // console.log(add(2, 3));
+    // // console.log(add(1));
 
 
-// console.log(add2(1));
-// console.log(add2(1, 2));
-// console.log(undefined);
+    // function add2(x, y = 10) {
+    //     console.log("length : " + arguments.length);
+    //     return x + y;
+    // }
 
+
+    // console.log(add2(1));
+    // console.log(add2(1, 2));
+    // console.log(undefined);
+}
 
 ///////////////////////////////////////// 16 - Spread Operator
 // https://youtu.be/Qxh55yYk1bU
 /////////////////////////////////////////
 
-// function print(x, y, z) {
-//     console.log(x);
-//     console.log(y);
-//     console.log(z);
-// }
+{
+    // function print(x, y, z) {
+    //     console.log(x);
+    //     console.log(y);
+    //     console.log(z);
+    // }
 
-// let map = new Map();
-// map.set("id", 333);
-// map.set("title", "hello");
+    // let map = new Map();
+    // map.set("id", 333);
+    // map.set("title", "hello");
 
-// let temp = [...map];
-// // console.log(temp[0][1]);
-// // console.log(temp[1][1]);
-// print(...temp);
+    // let temp = [...map];
+    // // console.log(temp[0][1]);
+    // // console.log(temp[1][1]);
+    // print(...temp);
 
 
-// let set = new Set([2, 4, 6]);
-// let nums = [...set];
+    // let set = new Set([2, 4, 6]);
+    // let nums = [...set];
 
-// print(...nums);
-
+    // print(...nums);
+}
 
 ///////////////////////////////////////// 15 - Rest Parameters
 // https://youtu.be/mIolXUjF3f4
 /////////////////////////////////////////
 
-// function print(x, y, ...values) {
+{
+    // function print(x, y, ...values) {
 
-//     console.log("value size : " + values.length);
+    //     console.log("value size : " + values.length);
 
-//     // console.log(x);
-//     // console.log(y);
+    //     // console.log(x);
+    //     // console.log(y);
 
-//     console.log(arguments[0]);
-//     console.log(arguments[1]);
-    
-//     for(let i=0; i < values.length; i++) {
-//         console.log(values[i]);
-//     }
-// }
+    //     console.log(arguments[0]);
+    //     console.log(arguments[1]);
+        
+    //     for(let i=0; i < values.length; i++) {
+    //         console.log(values[i]);
+    //     }
+    // }
 
-// print(2, 3, 5, 6, 7);
-// // print(2, 3, "hello", 5);
-
+    // print(2, 3, 5, 6, 7);
+    // // print(2, 3, "hello", 5);
+}
 
 ///////////////////////////////////////// 14 - Map 콜렉션
 // https://youtu.be/wP4oHNylsvM
 /////////////////////////////////////////
 
-// let map = new Map();
-// map.set("id", 1);
+{
+    // let map = new Map();
+    // map.set("id", 1);
 
-// console.log(map.size);
-// console.log("---------------");
+    // console.log(map.size);
+    // console.log("---------------");
 
-// map.set("title", "map이란?");
-// console.log(map.size);
-// console.log("---------------");
-// console.log(map.get("title"));
-
-
-// let exam = new Map([[1, "hello"], [2, "hi"], [4, "ok"]]);
-// exam.set("kor", 10);
-// exam.set("eng", 10);
-// exam.set("math", 10);
+    // map.set("title", "map이란?");
+    // console.log(map.size);
+    // console.log("---------------");
+    // console.log(map.get("title"));
 
 
-// for(let [k, v] of exam) {
-//     console.log(typeof v)
-//     console.log(k + " : " + v);
-// }
+    // let exam = new Map([[1, "hello"], [2, "hi"], [4, "ok"]]);
+    // exam.set("kor", 10);
+    // exam.set("eng", 10);
+    // exam.set("math", 10);
 
+
+    // for(let [k, v] of exam) {
+    //     console.log(typeof v)
+    //     console.log(k + " : " + v);
+    // }
+}
 
 ///////////////////////////////////////// 13 - Set 콜렉션
 // https://youtu.be/tUXMrY1nNqw
 /////////////////////////////////////////
+{
+    // Set - List - Map
 
-// Set - List - Map
+    // let set = new Set();
+    // // set.add(5);
+    // // set.add("5");
+    // // set.add(2);
+    // // set.add(5);
 
-// let set = new Set();
-// // set.add(5);
-// // set.add("5");
-// // set.add(2);
-// // set.add(5);
+    // set.add(5).add("5").add(2).add(5);
 
-// set.add(5).add("5").add(2).add(5);
-
-// console.log(set.size)
+    // console.log(set.size)
 
 
-// let arr = [10, 20, 30, 20, 30, 40, 30, 40, 50];
-// let set = new Set(arr);
+    // let arr = [10, 20, 30, 20, 30, 40, 30, 40, 50];
+    // let set = new Set(arr);
 
-// // console.log(set.size);
+    // // console.log(set.size);
 
-// // 값 나열하기
-// set.forEach(function(value, key, ownerSet) {
-//     console.log(key + " : " + value);
-// })
+    // // 값 나열하기
+    // set.forEach(function(value, key, ownerSet) {
+    //     console.log(key + " : " + value);
+    // })
 
-// // // for-of를 이용한 값 나열
-// // let set2 = new Set([2, 10, 7, 45, 23]);
-// // for(let v of set2) {
-// //     console.log(v);
-// // }
+    // // // for-of를 이용한 값 나열
+    // // let set2 = new Set([2, 10, 7, 45, 23]);
+    // // for(let v of set2) {
+    // //     console.log(v);
+    // // }
 
-// // for-of를 이용한 값 나열
-// let set3 = new Set([2, 10, 7, 45, 23]);
-// for(let [key, value] of set3.entries()) {
-//     console.log(key + " : " +  value);
-// }
+    // // for-of를 이용한 값 나열
+    // let set3 = new Set([2, 10, 7, 45, 23]);
+    // for(let [key, value] of set3.entries()) {
+    //     console.log(key + " : " +  value);
+    // }
 
-// let set4 = new Set();
-// set4.add(4).add(5);
+    // let set4 = new Set();
+    // set4.add(4).add(5);
 
-// console.log("----" + set4.size);
-// console.log(set4.has(4));
-// console.log(set4.has("4"));
+    // console.log("----" + set4.size);
+    // console.log(set4.has(4));
+    // console.log(set4.has("4"));
 
-// set4.delete(5);
-// console.log("----" + set4.size);
+    // set4.delete(5);
+    // console.log("----" + set4.size);
 
-// set4.add(6).add(7);
-// console.log("----" + set4.size);
+    // set4.add(6).add(7);
+    // console.log("----" + set4.size);
 
-// set4.clear();
-// console.log("----" + set4.size);
+    // set4.clear();
+    // console.log("----" + set4.size);
+}
 
 ///////////////////////////////////////// 12 - Array Destructuring #2
-// 
+// https://youtu.be/ksLga2iigCk
 /////////////////////////////////////////
 
-// let notice = {
-//     title : "공지사항",
-//     files : ["img1.png", "img3.png"]
-// };
+{
+    // let notice = {
+    //     title : "공지사항",
+    //     files : ["img1.png", "img3.png"]
+    // };
 
-// let {files:[first]} = notice;
+    // let {files:[first]} = notice;
 
-// console.log(first);
+    // console.log(first);
 
-// console.log(`---------------------------------`);
+    // console.log(`---------------------------------`);
 
-// let notices = {
-//     title : "공지사항",
-//     list : [
-//         {title:"오~", content:"내용무"},
-//         {title:"하~", content:"ㅋㅋ"}
-//     ]
-// };
+    // let notices = {
+    //     title : "공지사항",
+    //     list : [
+    //         {title:"오~", content:"내용무"},
+    //         {title:"하~", content:"ㅋㅋ"}
+    //     ]
+    // };
 
-// let {list:[notice1]} = notices;
-// let {list:[,notice2]} = notices;
+    // let {list:[notice1]} = notices;
+    // let {list:[,notice2]} = notices;
 
-// console.log(notice1.title);
-// console.log(notice2.title);
-
-
-
-
-
-
-///////////////////////////////////////// 11 - Array Destructuring #2
-// 
-/////////////////////////////////////////
+    // console.log(notice1.title);
+    // console.log(notice2.title);
+}
 
 {
     // let kors = [10, 20, 30];
@@ -444,10 +438,13 @@
 
     // console.log(com);
 
-    ///////////////////////////////////////// 11 - Array Destructuring #1
-    // 
-    /////////////////////////////////////////
+}
 
+ ///////////////////////////////////////// 11 - Array Destructuring #1
+ // https://youtu.be/0KRWqVWmhi0
+ /////////////////////////////////////////
+
+{
     // let kors = [10, 20, 30];
     // let [kor1, kor2] = kors;
 
@@ -458,8 +455,8 @@
     // let [,, kor3] = kors;
     // console.log(kor3);
 
-    // let kors = [10, 20, 30];
-    // let [kor1, kor2, kor3] = kors;
+    // // let kors = [10, 20, 30];
+    // // let [kor1, kor2, kor3] = kors;
 
 
     // console.log(kor1);
@@ -493,54 +490,53 @@
 }
 
 
-///////////////////////////////////////// 7. Object Destruncturing #3(뽀개기 중첩과 적응)
-// 
+///////////////////////////////////////// 10 - Object Destructuring #3 중첩과 적응
+// https://youtu.be/3EcJlwFyZ18
 /////////////////////////////////////////
 
 {
-// 객체의 속성과 다른 이름을 지정하려면?
+    // // 객체의 속성과 다른 이름을 지정하려면?
 
-// var exam = {
-//     kor:20,
-//     eng:30,
-//     math:40
-// };
+    // var exam = {
+    //     kor:20,
+    //     eng:30,
+    //     math:40
+    // };
 
-// // Destructuring
-// let {kor, eng} = exam;
-// let {kor:korean, eng:english} = exam; // 속성:{별칭}
+    // // Destructuring
+    // let {kor, eng} = exam;
+    // let {kor:korean, eng:english} = exam; // 속성:{별칭}
 
-// console.log(korean); // kor, eng는 사용 못함
-// console.log(english);
+    // console.log(korean); // kor, eng는 사용 못함
+    // console.log(english);
 
-// // 객체가 중첩된 경우
-// var exam = {
-//     kor:20,
-//     eng:30,
-//     math:40,
-//     student:{
-//         name : "sc",
-//         phone : "010-123-4567"
-//     }
-// };
+    // // 객체가 중첩된 경우
+    // var exam = {
+    //     kor:20,
+    //     eng:30,
+    //     math:40,
+    //     student:{
+    //         name : "sc",
+    //         phone : "010-123-4567"
+    //     }
+    // };
 
-// // let kor = exam.kor;
-// // let eng = exam.eng;
-// // let name = exam.student.name;
-// // let phone = exam.student.phone;
+    // // let kor = exam.kor;
+    // // let eng = exam.eng;
+    // let name = exam.student.name;
+    // let phone = exam.student.phone;
 
-// let {kor, eng, student:{name, phone}} = exam;
+    // // let {kor, eng, student:{name, phone}} = exam;
 
-// console.log(kor);
-// console.log(eng);
-// console.log(name);
-// console.log(phone);
-
+    // console.log(kor);
+    // console.log(eng);
+    // console.log(name);
+    // console.log(phone);
 
 }
 
-///////////////////////////////////////// 6. Object Destruncturing #2(속성확장과 기본값)
-// 
+///////////////////////////////////////// 09 - Object Destructuring #2
+// https://youtu.be/PElZVuzUP74
 /////////////////////////////////////////
 
 {
@@ -577,12 +573,11 @@
 
     // // function printExam(exam) {
     // function printExam({kor, eng, math}) {
-    // // 
 
     //     // Destruncturing 
-    //     let kor = exam.kor;
-    //     let eng = exam.eng;
-    //     let math = exam.math;
+    //     // let kor = exam.kor;
+    //     // let eng = exam.eng;
+    //     // let math = exam.math;
     //     // let {kor, eng, math} = exam;
 
     //     console.log("---- print() ")
